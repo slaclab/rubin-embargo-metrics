1e4c7a4403b77985be98fed030f036edecd19df4
yee379 <yee379@gmail.com>
2023-10-02 13:26:36 -0700

    feat: add aioredis

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, Request
 from prometheus_client import make_asgi_app, Counter
+import aioredis
 
+from os import environ
+import logging
+
+
+REDIS = None
 app = FastAPI()
 
 index_counter = Counter('index_counter', 'Description of counter')
@@ -8,9 +14,20 @@ index_counter = Counter('index_counter', 'Description of counter')
 metrics_app = make_asgi_app()
 app.mount("/metrics", metrics_app)
 
+@app.on_event('startup')
+async def startup_event():
+  REDIS = await aioredis.from_url( environ.get('REDIS_URL', 'redis://localhost/1') ) 
+
+@app.get("/keys")
+async def list():
+  # list all keys from redis store
+  async with REDIS.client() as redis:
+    keys = await redis.keys()
+  return f"KEYS: {keys}"
+
 @app.get("/")
 def index():
-    index_counter.inc()
-    return "Hello, world!"
+  index_counter.inc()
+  return f"REDIS STATE: {REDIS}"
 
 
